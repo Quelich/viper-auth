1c5649d8d4e2ef846e543230e7336f8d82d9a8dd
Emre Kılıç <emre1kilic@outlook.com>
2022-06-20 20:53:48 +0300

    add multi dex support

---
+++ lib/interactor/signin_interactor.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SigninInteractor extends StatefulWidget {
+  const SigninInteractor({Key? key}) : super(key: key);
+
+  @override
+  State<SigninInteractor> createState() => _SigninInteractorState();
+}
+
+class _SigninInteractorState extends State<SigninInteractor> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'auth_app.dart';
 
 void main() {
+  initializeFirebase();
   runApp(const AuthApp());
 }
 
+// Initialize Firebase before the app starts.
+Future<void> initializeFirebase() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+}
